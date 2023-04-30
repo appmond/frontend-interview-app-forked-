6d88dad668c47358ca7054a946e10c25b5b046b5
appmond <50201150+appmond@users.noreply.github.com>
2023-04-30 10:47:04 +0000

    Add RTK

--- src/buyflow/Buyflow.tsx
+++ src/buyflow/Buyflow.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-import AgeStep from "./AgeStep";
-import EmailStep from "./EmailStep";
-import SummaryStep from "./SummaryStep";
-import { useAppSelector, useAppDispatch } from "../state/hooks";
-import { buyflowActionCreators } from "../state";
-import { bindActionCreators } from "@reduxjs/toolkit";
+import React from 'react'
+import AgeStep from './AgeStep'
+import EmailStep from './EmailStep'
+import SummaryStep from './SummaryStep'
+import { useAppSelector, useAppDispatch } from '../state/hooks'
+import { buyflowActionCreators } from '../state'
+import { bindActionCreators } from '@reduxjs/toolkit'
 
 interface BuyflowProps {
-  productId: ProductIds;
+  productId: ProductIds
 }
 
 export enum ProductIds {
-  devIns = "dev_ins"
+  devIns = 'dev_ins',
 }
 
 const PRODUCT_IDS_TO_NAMES = {
-  [ProductIds.devIns]: "Developer Insurance"
-};
+  [ProductIds.devIns]: 'Developer Insurance',
+}
 
 const Buyflow: React.FC<BuyflowProps> = (props) => {
-  const dispatch = useAppDispatch();
-  const { currentStep } = useAppSelector((state) => state.buyflowReducer);
-  const { setAge, setEmail, setCurrentStep } = bindActionCreators(
+  const dispatch = useAppDispatch()
+  const { currentStep } = useAppSelector((state) => state.buyflowReducer)
+  const { setAge, setCurrentStep } = bindActionCreators(
     buyflowActionCreators,
     dispatch
-  );
+  )
   const getStepCallback = (nextStep: string) => (field: string, value: any) => {
-    updateData({ ...collectedData, [field]: value });
-    setCurrentStep(nextStep);
-  };
+    updateData({ ...collectedData, [field]: value })
+    setCurrentStep(nextStep)
+  }
   return (
     <>
       <h4>Buying {PRODUCT_IDS_TO_NAMES[props.productId]}</h4>
-      {(currentStep === "email" && <EmailStep cb={getStepCallback("age")} />) ||
-        (currentStep === "age" && (
-          <AgeStep cb={getStepCallback("summary")} />
+      {(currentStep === 'email' && <EmailStep cb={getStepCallback('age')} />) ||
+        (currentStep === 'age' && (
+          <AgeStep cb={getStepCallback('summary')} />
         )) ||
-        (currentStep === "summary" && (
+        (currentStep === 'summary' && (
           <SummaryStep collectedData={collectedData} />
         ))}
     </>
-  );
-};
+  )
+}
 
-export default Buyflow;
+export default Buyflow
